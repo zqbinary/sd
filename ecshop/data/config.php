de46e53f1c6929b56057becf16003540431af2a1
zqbinary <zqbinary@foxmail.com>
2017-11-18 21:25:45 +0800

    first commit

---
+++ ecshop/data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ss";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "123";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "Asia/Shanghai";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','43bb32340b973a81073d90a62f2cf0be');
+
+?>
