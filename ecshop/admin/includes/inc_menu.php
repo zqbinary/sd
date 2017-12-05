7964f1b58ff4f4aaa3d49a6bb3132d9bc88e091b
zqbinary <zqbinary@foxmail.com>
2017-12-05 21:38:00 +0800

    添加:菜单取代移动支付等

--- ecshop/admin/includes/inc_menu.php
+++ ecshop/admin/includes/inc_menu.php
@@ -153,11 +153,11 @@ $modules['16_email_manage']['magazine_list']        = 'magazine_list.php?act=lis
 $modules['16_email_manage']['attention_list']       = 'attention_list.php?act=list';
 $modules['16_email_manage']['view_sendlist']        = 'view_sendlist.php?act=list';
 
-$modules['18_lead_manage']['banner_mobile']        = 'mobile_setting.php?act=list';//移动端banner设置
-$modules['18_lead_manage']['lead']        = 'lead.php?act=list';//H5店铺二维码
-$modules['18_lead_manage']['leancloud']        = 'leancloud.php?act=list';//云推送管理
-$modules['18_lead_manage']['mobile_setting']        = 'ecmobile_setting.php?act=list';//移动版应用配置
-$modules['18_lead_manage']['h5_setting']        = 'h5_setting.php?act=list';//移动版应用配置
-$modules['18_lead_manage']['wxa_setting']        = 'wxa_setting.php?act=list';//小程序应用配置
+//$modules['18_lead_manage']['banner_mobile']        = 'mobile_setting.php?act=list';//移动端banner设置
+//$modules['18_lead_manage']['lead']        = 'lead.php?act=list';//H5店铺二维码
+//$modules['18_lead_manage']['leancloud']        = 'leancloud.php?act=list';//云推送管理
+//$modules['18_lead_manage']['mobile_setting']        = 'ecmobile_setting.php?act=list';//移动版应用配置
+//$modules['18_lead_manage']['h5_setting']        = 'h5_setting.php?act=list';//移动版应用配置
+//$modules['18_lead_manage']['wxa_setting']        = 'wxa_setting.php?act=list';//小程序应用配置
 
 ?>
